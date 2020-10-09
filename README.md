ca459c94e544a604e84862374a9a54a1775ddd96
unknown <thevoxium@gmail.com>
2020-10-09 12:00:02 +0530

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# Image-Super-Resolution" 
+# Image Super Resolution
+
+I have implemented state of the art SRCNN model to produce high resolution images from the given low resolution images. I have used Adam instead of SGD as proposed in the original paper because Adam is much faster than SGD.
+
+I have used PyTorch to implement the paper. I have also used Click to make a command line application for easy use.
--- run.py
+++ run.py
@@ -2,41 +2,36 @@ import click
 import numpy as np
 import torchvision.transforms as transforms
 from PIL import Image
-import torch
+
 device = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")
 print(device)
 
 
-@click.group()
+@click.group
 def cli():
     pass
 
 
-
-@cli.command()
+@cli.group()
+@click.command()
 @click.option('--model', type=str, required=True)
 @click.option('--image', type=str, required=True)
 @click.option('--zoom', type=int, required=True)
 def test(model, image, zoom):
-    img  = Image.open(image).convert('YCbCr')
+    img  = Image.Open(image).convert('YCbCr')
     img = img.resize((int(img.size[0]*zoom), int(img.size[1]*zoom)), Image.BICUBIC)
 
     y, cb, cr = img.split()
     img_tensor = transforms.ToTensor()
     input = img_tensor(y).view(1, -1, y.size[1], y.size[0])
-    model = torch.load(model).to(device)
+
     input = input.to(device)
 
     out = model(input)
-    out = out.cpu()
     out_img_y = out[0].detach().numpy()
     out_img_y *= 255.0
     out_img_y = out_img_y.clip(0, 255)
     out_img_y = Image.fromarray(np.uint8(out_img_y[0]), mode='L')
 
     out_img = Image.merge('YCbCr', [out_img_y, cb, cr]).convert('RGB')
-    out_img.save(f"zoomed_image.jpg")
-
-
-if __name__ == '__main__':
-    cli()
+    out_img.save(f"zoomed_{args.image}")
